ad86bbe29694f398a18fecf2e8f0ef70b8c03811
Adeline Hillier <adelineh2000@gmail.com>
2021-09-02 13:34:58 -0400

    Annotate `examples/quick_calibrate.jl`

--- examples/quick_calibrate.jl
+++ examples/quick_calibrate.jl
@@ -9,9 +9,12 @@ using OceanTurbulenceParameterEstimation.CATKEVerticalDiffusivityModel
 
 using OceanBoundaryLayerParameterizations
 
+# CATKE parameters involved in setting field diffusivities 
+StabilityFnParameters = @free_parameters CᴷRiʷ, CᴷRiᶜ, Cᴷu⁻, Cᴷuʳ, Cᴷc⁻, Cᴷcʳ, Cᴷe⁻, Cᴷeʳ,
+
 parameters = Parameters(
     RelevantParameters = TKEParametersRiDependent,  # Parameters that are used in CATKE
-    ParametersToOptimize = TKEParametersRiDependent # Subset of RelevantParameters that we want to optimize
+    ParametersToOptimize = StabilityFnParameters    # Subset of RelevantParameters that we want to optimize
 )
 
 # DataSet represents the Model, Data, and loss function
@@ -30,10 +33,6 @@ calibration = DataSet(FourDaySuite, # "Truth data" for model calibration
                       Nz = 16,
                       Δt = 30.0)
 
-#=
-
-
-
 validation = DataSet(merge(TwoDaySuite, SixDaySuite), p;
                      relative_weights = relative_weight_options["all_but_e"],
                      ensemble_size = 10,
@@ -50,21 +49,25 @@ l0 = calibration()
 #   3. a vector of parameter vectors (one for each ensemble member)
 #   4. or a vector of FreeParameter objects (one for each ensemble member)
 # If (1) or (2), the ensemble members are redundant and the loss is computed for just the one parameter set.
-
 lθ = calibration(θ)
-=#
+
+# Output files/figures
+directory = joinpath(pwd(), "quick_calibrate_results")
 
 # Example parameters
 θ = calibration.default_parameters
 
+# Run the model forward and store the solution
 output = model_time_series(calibration, θ)
 
-visualize_realizations(calibration, θ)
+# Run the model forward with parameters θ and visualize the solution compared to the truth
+visualize_realizations(calibration, θ; filename = "example_quick_calibrate.png")
 
-visualize_and_save(calibration, validation, default_parameters, pwd())
+# Runs `visualize_realizations` and records a summary of the calibration results in a `result.txt` file.
+visualize_and_save(calibration, validation, default_parameters, directory)
 
 # Use EKI to calibrate a one-dimensional loss function
-eki_unidimensional(loss::DataSet, initial_parameters;
+eki(loss::DataSet, initial_parameters;
                    noise_level = 10^(-2.0),
                    N_ens = 10,
                    N_iter = 15,
