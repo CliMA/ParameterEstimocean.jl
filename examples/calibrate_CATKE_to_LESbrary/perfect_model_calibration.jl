3114221f5feadebcca87b1bb807087424645b743
Adeline Hillier <adelineh2000@gmail.com>
2021-11-04 11:53:59 -0400

    Debug perfect_model_calibration.jl

--- examples/calibrate_CATKE_to_LESbrary/perfect_model_calibration.jl
+++ examples/calibrate_CATKE_to_LESbrary/perfect_model_calibration.jl
@@ -9,6 +9,7 @@ using OceanTurbulenceParameterEstimation
 
 include("lesbrary_paths.jl")
 include("parameters.jl")
+include("visualize_profile_predictions.jl")
 
 #####
 ##### Simulation parameters
@@ -16,16 +17,11 @@ include("parameters.jl")
 
 Nz = 32
 Lz = 64
-Qᵇ = 6e-8
-Qᵘ = -7e-4
 Δt = 10.0
-f₀ = 1e-4
 N² = 1e-5
 stop_time = 3hour
 save_interval = 1hour
-experiment_name = "catke_perfect_model_observation"
-data_path = experiment_name * ".jld2"
-ensemble_size = 50
+ensemble_size = 30
 generate_observations = true
 
 #####
@@ -38,8 +34,8 @@ closure = closure_with_parameter_set(CATKEVerticalDiffusivity(Float64;), paramet
 true_parameters = (Cᵟu = 0.5, CᴷRiʷ = 1.0, Cᵂu★ = 2.0, CᵂwΔ = 1.0, Cᴷeʳ = 5.0, Cᵟc = 0.5, Cᴰ = 2.0, Cᴷc⁻ = 0.5, Cᴷe⁻ = 0.2, Cᴷcʳ = 3.0, Cᴸᵇ = 1.0, CᴷRiᶜ = 1.0, Cᴷuʳ = 4.0, Cᴷu⁻ = 1.2, Cᵟe = 0.5)
 true_closure = closure_with_parameters(closure, true_parameters)
 
-if generate_observations || !(isfile(data_path))
-
+function generate_truth_data!(name; Qᵘ, Qᵇ, f₀)
+    
     grid = RegularRectilinearGrid(size=Nz, z=(-Lz, 0), topology=(Flat, Flat, Bounded))
 
     u_bcs = FieldBoundaryConditions(top = FluxBoundaryCondition(Qᵘ))
@@ -58,7 +54,7 @@ if generate_observations || !(isfile(data_path))
     
     simulation.output_writers[:fields] = JLD2OutputWriter(model, merge(model.velocities, model.tracers),
                                                           schedule = TimeInterval(save_interval),
-                                                          prefix = experiment_name,
+                                                          prefix = name,
                                                           array_type = Array{Float64},
                                                           field_slicer = nothing,
                                                           force = true)
@@ -66,40 +62,23 @@ if generate_observations || !(isfile(data_path))
     run!(simulation)
 end
 
-data_path2 = "catke_perfect_model_observation2.jld2"
+experiment_name = "catke_perfect_model_observation"
+data_path = experiment_name * ".jld2"
+
+if generate_observations || !(isfile(data_path))
+    generate_truth_data!(experiment_name; Qᵘ=-7e-4, Qᵇ=6e-8, f₀=1e-4)
+end
+
+experiment_name2 = "catke_perfect_model_observation2"
+data_path2 = experiment_name2 * ".jld2"
+
 if generate_observations || !(isfile(data_path2))
-
-    grid = RegularRectilinearGrid(size=Nz, z=(-Lz, 0), topology=(Flat, Flat, Bounded))
-
-    u_bcs = FieldBoundaryConditions(top = FluxBoundaryCondition(Qᵘ))
-    b_bcs = FieldBoundaryConditions(top = FluxBoundaryCondition(Qᵇ*0.5), bottom = GradientBoundaryCondition(N²))
-
-    model = HydrostaticFreeSurfaceModel(grid = grid,
-                                        tracers = (:b, :e),
-                                        buoyancy = BuoyancyTracer(),
-                                        boundary_conditions = (; u=u_bcs, b=b_bcs),
-                                        coriolis = FPlane(f=f₀),
-                                        closure = true_closure)
-                                        
-    set!(model, b = (x, y, z) -> N² * z)
-    
-    simulation = Simulation(model; Δt, stop_time)
-    
-    simulation.output_writers[:fields] = JLD2OutputWriter(model, merge(model.velocities, model.tracers),
-                                                          schedule = TimeInterval(save_interval),
-                                                          prefix = "catke_perfect_model_observation2",
-                                                          array_type = Array{Float64},
-                                                          field_slicer = nothing,
-                                                          force = true)
-    
-    run!(simulation)
+    generate_truth_data!(experiment_name2; Qᵘ=-1.4e-3, Qᵇ=3e-8, f₀=1.5e-4)
 end
 
 #####
 ##### Load truth data as observations
 #####
-
-data_path = experiment_name * ".jld2"
 
 # observations = SixDaySuite("/Users/adelinehillier/.julia/dev")
 
@@ -120,7 +99,6 @@ closure_ensemble = [closure for i = 1:Nx, j = 1:Ny]
 
 function get_parameter(observation, parameter_path)
     file = jldopen(observation.path)
-    # parameter = getproperty(file, parameter_path)
     parameter = file[parameter_path]
     close(file)
     return parameter
@@ -134,7 +112,7 @@ Qᵇ_ensemble = [get_Qᵇ(observations[j]) for i = 1:Nx, j = 1:Ny]
 Qᵘ_ensemble = [get_Qᵘ(observations[j]) for i = 1:Nx, j = 1:Ny]
 
 ensemble_b_bcs = FieldBoundaryConditions(top = FluxBoundaryCondition(Qᵇ_ensemble), bottom = GradientBoundaryCondition(N²))
-ensemble_u_bcs = FieldBoundaryConditions(top = FluxBoundaryCondition(Qᵘ))
+ensemble_u_bcs = FieldBoundaryConditions(top = FluxBoundaryCondition(Qᵘ_ensemble))
 coriolis_ensemble = [FPlane(f=get_f₀(observations[j])) for i = 1:Nx, j = 1:Ny]
 
 ensemble_model = HydrostaticFreeSurfaceModel(grid = ensemble_grid,
@@ -150,29 +128,15 @@ ensemble_simulation = Simulation(ensemble_model; Δt, stop_time)
 
 pop!(ensemble_simulation.diagnostics, :nan_checker)
 
-# #####
-# ##### Set up ensemble model
-# #####
-
-# ensemble_model = OneDimensionalEnsembleModel(observations; 
-#                        architecture = CPU(), 
-#                        ensemble_size = 50, 
-#                        closure = closure
-#                       )
-
-# ensemble_simulation = Simulation(ensemble_model; Δt, stop_time)
-
-# pop!(ensemble_simulation.diagnostics, :nan_checker)
-
 #####
 ##### Build free parameters
 #####
 
 free_parameter_names = keys(parameter_set.defaults)
-free_parameter_means = [values(parameter_set.defaults)...]
+free_parameter_means = collect(values(parameter_set.defaults))
 priors = NamedTuple(pname => ConstrainedNormal(0.0, 1.0, bounds(pname)...) for pname in free_parameter_names)
 
-θ★ = [values(true_parameters)...]
+θ★ = collect(values(true_parameters))
 
 free_parameters = FreeParameters(priors)
 
@@ -180,9 +144,17 @@ free_parameters = FreeParameters(priors)
 ##### Build the Inverse Problem
 #####
 
-calibration = InverseProblem(observations, ensemble_simulation, free_parameters)
+calibration = InverseProblem(observations, ensemble_simulation, free_parameters);
+@show calibration.simulation.model.closure[1,1] == true_closure
+@show calibration.simulation.model.closure[10,2] == true_closure
 
-a = forward_map(calibration, free_parameter_means)
-x = forward_map(calibration, θ★)
+# a = forward_map(calibration, free_parameter_means)
+x = forward_map(calibration, θ★)[:,1:1]
 y = observation_map(calibration)
+@show x == y
 
+visualize!(calibration, θ★; 
+                    field_names = [:u, :v, :b, :e],
+                    directory = joinpath(pwd(), "examples/calibrate_CATKE_to_LESbrary"),
+                    filename = "catke_perfect_model_visual.png"
+                    )
