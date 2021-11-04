359c1d9c4461a210892ab2820c36767984abf3bb
Navid C. Constantinou <navidcy@users.noreply.github.com>
2021-11-04 12:01:16 +1100

    fix baroclinic adjustment perfect model calibration

--- examples/baroclinic_adjustment_perfect_model_calibration.jl
+++ examples/baroclinic_adjustment_perfect_model_calibration.jl
@@ -5,91 +5,63 @@ using Printf
 using Oceananigans
 using Oceananigans.Units
 using Oceananigans.Models.HydrostaticFreeSurfaceModels: SliceEnsembleSize
-using Oceananigans: fields
-using Oceananigans.TurbulenceClosures: VerticallyImplicitTimeDiscretization
 using OceanTurbulenceParameterEstimation
 
 #####
 ##### Parameters
 #####
 
+# "True" parameters to be estimated by calibration
+κ_skew = 1000.0       # [m² s⁻¹] skew diffusivity
+κ_symmetric = 900.0   # [m² s⁻¹] symmetric diffusivity
 
 # Domain
-Lx = 500kilometers  # east-west extent [m]
 Ly = 1000kilometers # north-south extent [m]
 Lz = 1kilometers    # depth [m]
 
-Nx = 1
 Ny = 64
-Nz = 32
+Nz = 16
 
 architecture = CPU()
 
-stop_time = 10days
-Δt = 15seconds
-save_interval = 1days
+stop_time = 1days
+save_interval = 0.25days
 experiment_name = "baroclinic_adjustment"
 data_path = experiment_name * ".jld2"
-ensemble_size = 30
-generate_observations = false
-
-# "True" parameters to be estimated by calibration
-κ_skew = 1000.0       # [m² s⁻¹] skew diffusivity
-κ_symmetric = 900.0  # [m² s⁻¹] symmetric diffusivity
-
-Δx, Δy, Δz = Lx/Nx, Ly/Ny, Lz/Nz
-
-𝒜 = Δz/Δx # Grid cell aspect ratio.
-
-κh = 0.1    # [m² s⁻¹] horizontal diffusivity
-νh = 0.1    # [m² s⁻¹] horizontal viscosity
-κz = 𝒜 * κh # [m² s⁻¹] vertical diffusivity
-νz = 𝒜 * νh # [m² s⁻¹] vertical viscosity
+ensemble_size = 10
+generate_observations = true
+Δt = 10minute
 
 θ★ = [κ_skew, κ_symmetric]
 
-diffusive_closure = AnisotropicDiffusivity(νh = νh,
-                                           νz = νz,
-                                           κh = κh,
-                                           κz = κz,
-                                           time_discretization = VerticallyImplicitTimeDiscretization())
-
-convective_adjustment = ConvectiveAdjustmentVerticalDiffusivity(convective_κz = 1.0,
-                                                                convective_νz = 0.0)
-
-gerdes_koberle_willebrand_tapering = Oceananigans.TurbulenceClosures.FluxTapering(1e-1)
+gerdes_koberle_willebrand_tapering = Oceananigans.TurbulenceClosures.FluxTapering(1e-2)
 
 gent_mcwilliams_diffusivity = IsopycnalSkewSymmetricDiffusivity(κ_skew = κ_skew,
                                                                 κ_symmetric = κ_symmetric,
                                                                 slope_limiter = gerdes_koberle_willebrand_tapering)
                                         
-closures = (diffusive_closure, convective_adjustment, gent_mcwilliams_diffusivity)
-
-closures = gent_mcwilliams_diffusivity
-
+coriolis = BetaPlane(latitude=-45)
 
 #####
 ##### Generate synthetic observations
 #####
 
-if generate_observations || !(isfile(data_path))
+if generate_observations
     grid = RegularRectilinearGrid(topology = (Flat, Bounded, Bounded), 
                                   size = (Ny, Nz), 
                                   y = (-Ly/2, Ly/2),
                                   z = (-Lz, 0),
                                   halo = (3, 3))
-
-    coriolis = BetaPlane(latitude=-45)
     
     model = HydrostaticFreeSurfaceModel(architecture = architecture,
                                         grid = grid,
                                         coriolis = coriolis,
                                         buoyancy = BuoyancyTracer(),
-                                        closure = closures,
+                                        closure = gent_mcwilliams_diffusivity,
                                         tracers = (:b, :c),
-                                        momentum_advection = WENO5(),
-                                        tracer_advection = WENO5(),
-                                        free_surface = ExplicitFreeSurface())
+                                        # momentum_advection = WENO5(),
+                                        # tracer_advection = WENO5(),
+                                        free_surface = ImplicitFreeSurface())
     
     @info "Built $model."
 
@@ -117,7 +89,6 @@ if generate_observations || !(isfile(data_path))
 
     Δc = 2Δy
     Δb = Δy * M²
-    ϵb = 1e-2 * Δb # noise amplitude
 
     bᵢ(x, y, z) = N² * z + Δb * ramp(y, Δy)
     cᵢ(x, y, z) = exp(-y^2 / 2Δc^2) * exp(-(z + Lz/2)^2 / (2*Δz^2))
@@ -141,7 +112,7 @@ if generate_observations || !(isfile(data_path))
         return nothing
     end
     
-    simulation = Simulation(model, Δt=Δt, stop_time=stop_time, progress=print_progress, iteration_interval=1000)
+    simulation = Simulation(model, Δt=Δt, stop_time=stop_time, progress=print_progress, iteration_interval=100)
     
     simulation.output_writers[:fields] = JLD2OutputWriter(model, merge(model.velocities, model.tracers),
                                                           schedule = TimeInterval(save_interval),
@@ -149,6 +120,8 @@ if generate_observations || !(isfile(data_path))
                                                           array_type = Array{Float64},
                                                           field_slicer = nothing,
                                                           force = true)
+
+    pop!(simulation.diagnostics, :nan_checker)
     
     run!(simulation)
 end
@@ -162,18 +135,17 @@ using GLMakie
 
 fig = Figure(resolution = (1400, 700))
 
-filepath = "baroclinic_adjustment.jld2"
+filepath = experiment_name * ".jld2"
 
 ut = FieldTimeSeries(filepath, "u")
 bt = FieldTimeSeries(filepath, "b")
 ct = FieldTimeSeries(filepath, "c")
 
-grid = RegularRectilinearGrid(topology = (Periodic, Bounded, Bounded), 
-                                  size = (Nx, Ny, Nz), 
-                                  x = (0, Lx),
+grid = RegularRectilinearGrid(topology = (Flat, Bounded, Bounded), 
+                                  size = (Ny, Nz),
                                   y = (-Ly/2, Ly/2),
                                   z = (-Lz, 0),
-                                  halo = (3, 3, 3))
+                                  halo = (2, 2))
 
 # Build coordinates, rescaling the vertical coordinate
 x, y, z = nodes((Center, Center, Center), grid)
@@ -234,53 +206,24 @@ observations = OneDimensionalTimeSeries(data_path, field_names=(:b, :c), normali
 #####
 
 slice_ensemble_size = SliceEnsembleSize(size=(Ny, Nz), ensemble=ensemble_size)
-@show ensemble_grid = RegularRectilinearGrid(topology = (Flat, Bounded, Bounded),
-                                             size = slice_ensemble_size,
-                                             y = (-Ly/2, Ly/2),
-                                             z = (-Lz, 0),
-                                             halo=(3, 3))
+@show ensemble_grid = RegularRectilinearGrid(size=slice_ensemble_size, y = (-Ly/2, Ly/2), z = (-Lz, 0), topology = (Flat, Bounded, Bounded), halo=(3, 3))
 
-closure_ensemble = [deepcopy(closures) for i = 1:ensemble_size] 
-coriolis_ensemble = [BetaPlane(latitude=-45) for i = 1:ensemble_size]
+closure_ensemble = [deepcopy(gent_mcwilliams_diffusivity) for i = 1:ensemble_size] 
 
 ensemble_model = HydrostaticFreeSurfaceModel(architecture = architecture,
                                              grid = ensemble_grid,
                                              tracers = (:b, :c),
                                              buoyancy = BuoyancyTracer(),
-                                             coriolis = coriolis_ensemble,
+                                             coriolis = coriolis,
                                              closure = closure_ensemble,
-                                             momentum_advection = WENO5(),
-                                             tracer_advection = WENO5(),
-                                             free_surface = ExplicitFreeSurface())
+                                             free_surface = ImplicitFreeSurface())
 
-wall_clock = [time_ns()]
-    
-function print_progress(sim)
-    @printf("[%05.2f%%] i: %d, t: %s, wall time: %s, max(u): (%6.8e, %6.8e, %6.8e) m/s\n",
-            100 * (sim.model.clock.time / sim.stop_time),
-            sim.model.clock.iteration,
-            prettytime(sim.model.clock.time),
-            prettytime(1e-9 * (time_ns() - wall_clock[1])),
-            maximum(abs, sim.model.velocities.u),
-            maximum(abs, sim.model.velocities.v),
-            maximum(abs, sim.model.velocities.w))
-
-    wall_clock[1] = time_ns()
-    
-    return nothing
-end
-
-ensemble_simulation = Simulation(ensemble_model; Δt=Δt, stop_time=stop_time, progress=print_progress, iteration_interval=1000)
+ensemble_simulation = Simulation(ensemble_model; Δt, stop_time)
 pop!(ensemble_simulation.diagnostics, :nan_checker)
 
 #####
 ##### Build free parameters
 #####
-
-# priors = (
-#     κ_skew = lognormal_with_mean_std(900, 200),
-#     κ_symmetric = lognormal_with_mean_std(1100, 200),
-# )
 
 priors = (
     κ_skew = ConstrainedNormal(0.0, 1.0, 400.0, 1300.0),
@@ -289,13 +232,26 @@ priors = (
 
 free_parameters = FreeParameters(priors)
 
+#####
+##### Build the Inverse Problem
+#####
+
+calibration = InverseProblem(observations, ensemble_simulation, free_parameters)
+
+x = forward_map(calibration, [θ★ for _ in 1:ensemble_size])
+y = observation_map(calibration)
+
+# # Assert that G(θ*) ≈ y
+@show x[:, 1:1] == y
+
 ###
 ### Visualize the prior densities
 ###
+#=
 using CairoMakie
 using OceanTurbulenceParameterEstimation.EnsembleKalmanInversions: convert_prior, inverse_parameter_transform
 
-samples(prior) = [inverse_parameter_transform(prior, x) for x in rand(convert_prior(prior), 100000000)]
+samples(prior) = [inverse_parameter_transform(prior, x) for x in rand(convert_prior(prior), 10000000)]
 samples_κ_skew = samples(priors.κ_skew)
 samples_κ_symmetric = samples(priors.κ_symmetric)
 
@@ -306,33 +262,22 @@ push!(densities, CairoMakie.density!(axtop, samples_κ_skew))
 push!(densities, CairoMakie.density!(axtop, samples_κ_symmetric))
 leg = Legend(f[1, 2], densities, ["κ_skew", "κ_symmetric"], position = :lb)
 # CairoMakie.xlims!(0, 2e-5)
-save("visualize_prior_kappa_skew.png", f)
+save("visualize_prior_kappa_skew_test.pdf", f)
 display(f)
+=#
 
-
-#####
-##### Build the Inverse Problem
-#####
-
-calibration = InverseProblem(observations, ensemble_simulation, free_parameters)
-
-# forward_map(calibration, [θ★ for _ in 1:ensemble_size])
-# x = forward_map(calibration, [θ★ for _ in 1:ensemble_size])
-# y = observation_map(calibration)
-
-# Assert that G(θ*) ≈ y
-# @show x[:, 1:1] == y
 
 #=
 using Plots, LinearAlgebra
 p = Plots.plot(collect(1:length(x)), [x...], label="forward_map")
 Plots.plot!(collect(1:length(y)), [y...], label="observation_map")
-# savefig(p, "obs_vs_pred.png")
+# savefig(p, "obs_vs_pred_test.pdf")
 display(p)
 =#
 
-iterations = 20
+iterations = 5
 eki = EnsembleKalmanInversion(calibration; noise_covariance = 1e-2)
+
 params = iterate!(eki; iterations = iterations)
 
 @show params
@@ -341,6 +286,7 @@ params = iterate!(eki; iterations = iterations)
 ### Summary plots
 ###
 
+using CairoMakie
 using LinearAlgebra
 
 θ̅(iteration) = [eki.iteration_summaries[iteration].ensemble_mean...]
@@ -362,7 +308,7 @@ for (i, pname) in enumerate(free_parameters.names)
     CairoMakie.lines!(ax3, 1:iterations, ev / ev[1], label=String(pname), linewidth = 2)
 end
 CairoMakie.axislegend(ax3, position = :rt)
-CairoMakie.save("summary_makie.pdf", f)
+CairoMakie.save("summary_makie_test.pdf", f)
 
 ###
 ### Plot ensemble density with time
@@ -374,7 +320,7 @@ axmain = CairoMakie.Axis(f[2, 1], xlabel = "κ_skew", ylabel = "κ_symmetric")
 axright = CairoMakie.Axis(f[2, 2])
 s = eki.iteration_summaries
 scatters = []
-for i in [1, 2, 5, 21]
+for i in [1, 2, 5, 11]
     ensemble = transpose(s[i].parameters)
     push!(scatters, CairoMakie.scatter!(axmain, ensemble))
     CairoMakie.density!(axtop, ensemble[:, 1])
@@ -397,12 +343,4 @@ CairoMakie.ylims!(axmain, 600, 1600)
 CairoMakie.ylims!(axright, 600, 1600)
 CairoMakie.xlims!(axright, 0, 0.06)
 CairoMakie.ylims!(axtop, 0, 0.06)
-save("distributions_makie.pdf", f)
-
-tupified_params = NamedTuple{calibration.free_parameters.names}(Tuple(params))
-
-OceanTurbulenceParameterEstimation.InverseProblems.run_simulation_with_params!(calibration, [tupified_params for _ in 1:ensemble_size])
-
-model_time_series = OceanTurbulenceParameterEstimation.InverseProblems.transpose_model_output(calibration.time_series_collector, calibration.observations)
-
-model_time_series.field_time_serieses.b
+save("distributions_makie_test.pdf", f)
