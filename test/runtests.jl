0c1aa72ed9eba6cedd1589cc848941e6874df0db
Navid C. Constantinou <navidcy@users.noreply.github.com>
2021-12-13 12:35:10 +1100

    add Doctests in testsets

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,10 @@
-using Test
+using OceanTurbulenceParameterEstimation, Test, Documenter, Distributions
 
 include("test_synthetic_observations.jl")
 include("test_forward_map.jl")
 include("test_ensemble_column_models.jl")
 include("test_turbulence_closure_parameters.jl")
+
+@testset "Doctests" begin
+    doctest(OceanTurbulenceParameterEstimation)
+end
