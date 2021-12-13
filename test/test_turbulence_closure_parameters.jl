11c8848fd85dcd6fb9209147ff0c7317fb7a4f83
Gregory Wagner <wagner.greg@gmail.com>
2021-12-13 14:22:08 -0700

    Fixes closure_with_parameters test

--- test/test_turbulence_closure_parameters.jl
+++ test/test_turbulence_closure_parameters.jl
@@ -1,3 +1,5 @@
+using Test
+using OceanTurbulenceParameterEstimation
 using Oceananigans
 using Oceananigans.Models.HydrostaticFreeSurfaceModels: ColumnEnsembleSize
 using Oceananigans.TurbulenceClosures: ConvectiveAdjustmentVerticalDiffusivity
@@ -10,10 +12,6 @@ const CAVD = ConvectiveAdjustmentVerticalDiffusivity
 const CATKE = CATKEVerticalDiffusivity
 
 @testset "TurbulenceClosureParameters tests" begin
-    Nz = 16
-    Hz = 1
-    grid = RectilinearGrid(size=Nz, z=(-10, 10), topology=(Flat, Flat, Bounded), halo=1)
-    
     @info "Testing closure_with_parameters on ConvectiveAdjustmentVerticalDiffusivity"
     closures = [CAVD(background_κz=1.0) CAVD(background_κz=1.1)
                 CAVD(background_κz=1.2) CAVD(background_κz=1.3)
@@ -29,35 +27,30 @@ const CATKE = CATKEVerticalDiffusivity
     end
 
     @info "Testing closure_with_parameters on CATKEVerticalDiffusivity"
-
     catke_closure = @suppress CATKE()
     new_Cᴷuʳ = 1e3
-    mixing_length = MixingLength(Cᴷuʳ = new_Cᴷuʳ)
-    new_catke_closure = closure_with_parameters(catke_closure, (mixing_length = mixing_length,))
+    new_catke_closure = closure_with_parameters(catke_closure, (; Cᴷuʳ = new_Cᴷuʳ))
     @test new_catke_closure.mixing_length.Cᴷuʳ == new_Cᴷuʳ
 
+    @info "Testing update_closure_ensemble_member! on ConvectiveAdjustmentVerticalDiffusivity"
     new_background_κz, new_convective_νz = 100.0, 10.0
-
     ensemble_member_no = 2
     update_closure_ensemble_member!(closures, ensemble_member_no, (background_κz = new_background_κz, convective_νz = new_convective_νz))
 
-    @info "Testing update_closure_ensemble_member! on ConvectiveAdjustmentVerticalDiffusivity"
     for j in 1:2
         @test closures[ensemble_member_no, j].background_κz == new_background_κz
         @test closures[ensemble_member_no, j].convective_νz == new_convective_νz
     end
 
-    new_CᴷRiʷ = 100.0
-    mixing_length = MixingLength(CᴷRiʷ = new_CᴷRiʷ)
-
+    @info "Testing update_closure_ensemble_member! on CATKEVerticalDiffusivity"
     closures = @suppress [CATKE() CATKE()
                           CATKE() CATKE()
                           CATKE() CATKE()]
 
+    new_CᴷRiʷ = 100.0
     ensemble_member_no = 2
-    update_closure_ensemble_member!(closures, ensemble_member_no, (mixing_length = mixing_length,))
+    update_closure_ensemble_member!(closures, ensemble_member_no, (; CᴷRiʷ = new_CᴷRiʷ))
 
-    @info "Testing update_closure_ensemble_member! on CATKEVerticalDiffusivity"
     for j in 1:2
         @test closures[ensemble_member_no, j].mixing_length.CᴷRiʷ == new_CᴷRiʷ
     end
