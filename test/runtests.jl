43feb32b213649497e3896701d0436d1d4804722
Navid C. Constantinou <navidcy@users.noreply.github.com>
2021-12-15 10:56:45 +1100

    remove some tests to check how codecov will behave

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,5 @@
 using Documenter, Distributions, Test
 
-include("test_synthetic_observations.jl")
-include("test_forward_map.jl")
-include("test_ensemble_column_models.jl")
 include("test_turbulence_closure_parameters.jl")
 
 @testset "Doctests" begin
