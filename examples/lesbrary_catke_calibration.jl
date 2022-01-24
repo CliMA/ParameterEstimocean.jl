d4febba1f6338a6413c43bb5e24fdfe342b9e146
Gregory Wagner <wagner.greg@gmail.com>
2022-01-24 10:03:18 -0700

    Major improvements

--- examples/lesbrary_catke_calibration.jl
+++ examples/lesbrary_catke_calibration.jl
@@ -10,9 +10,7 @@
 using Oceananigans
 using Oceananigans.Units
 using OceanTurbulenceParameterEstimation
-using LinearAlgebra
-using CairoMakie
-using DataDeps
+using LinearAlgebra, CairoMakie, DataDeps
 
 using Oceananigans.TurbulenceClosures.CATKEVerticalDiffusivities: CATKEVerticalDiffusivity, MixingLength
 
@@ -23,13 +21,10 @@ using Oceananigans.TurbulenceClosures.CATKEVerticalDiffusivities: CATKEVerticalD
 # calibration of a turbulence parameterization to one of these simulations:
 
 data_path = datadep"two_day_suite_4m/strong_wind_instantaneous_statistics.jld2"
+times = [2hours, 6hours, 12hours]
+field_names = (:u, :v, :b, :e)
 
-times = [2hours, 6hours, 18hours]
-
-observations = SyntheticObservations(data_path;
-                                     field_names = (:u, :v, :b, :e),
-                                     normalize = ZScore,
-                                     times)
+observations = SyntheticObservations(data_path; field_names, times, normalize=ZScore)
 
 # Let's take a look at the observations. We define a few
 # plotting utilities along the way to use later in the example:
@@ -38,50 +33,47 @@ colorcycle = [:black, :red, :darkblue, :orange, :pink1, :seagreen, :magenta2]
 markercycle = [:rect, :utriangle, :star5, :circle, :cross, :+, :pentagon]
 
 function make_figure_axes()
-    fig = Figure()
-    ax_b = Axis(fig[1, 1], xlabel = "Buoyancy\n[10⁻⁴ m s⁻²]", ylabel = "z [m]")
-    ax_u = Axis(fig[1, 2], xlabel = "Velocities\n[cm s⁻¹]")
-    ax_e = Axis(fig[1, 3], xlabel = "Turbulent kinetic energy\n[10⁻⁴ m² s⁻²]")
-    return fig, (ax_b, ax_u, ax_e)
+    fig = Figure(resolution=(1200, 400))
+    ax_b = Axis(fig[1, 1], xlabel = "Buoyancy \n[10⁻⁴ m s⁻²]", ylabel = "z [m]")
+    ax_u = Axis(fig[1, 2], xlabel = "x-velocity, u \n[cm s⁻¹]")
+    ax_v = Axis(fig[1, 3], xlabel = "y-velocity, v \n[cm s⁻¹]")
+    ax_e = Axis(fig[1, 4], xlabel = "Turbulent kinetic energy\n[10⁻⁴ m² s⁻²]")
+    return fig, (ax_b, ax_u, ax_v, ax_e)
 end
 
-function plot_fields!(axs, b, u, v, e, label, u_label, v_label, color)
+function plot_fields!(axs, b, u, v, e, label, color)
     z = znodes(Center, observations.grid)
-    lines!(axs[1], 1e4 * interior(b)[1, 1, :], z; label, color) # convert units m s⁻² -> 10⁻⁴ m s⁻²
-    lines!(axs[2], 1e2 * interior(u)[1, 1, :], z; linestyle=:solid, color, label=u_label) # convert units m s⁻¹ -> cm s⁻¹
-    lines!(axs[2], 1e2 * interior(v)[1, 1, :], z; linestyle=:dash, color, label=v_label) # convert units m s⁻¹ -> cm s⁻¹
-    lines!(axs[3], 1e4 * interior(e)[1, 1, :], z; label, color) # convert units m² s⁻² -> 10⁻⁴ m² s⁻²
+    # Note unit conversions below, eg m s⁻² -> 10⁻⁴ m s⁻²:
+    lines!(axs[1], 1e4 * interior(b)[1, 1, :], z; color, label)
+    lines!(axs[2], 1e2 * interior(u)[1, 1, :], z; color, label)
+    lines!(axs[3], 1e2 * interior(v)[1, 1, :], z; color, label)
+    lines!(axs[4], 1e4 * interior(e)[1, 1, :], z; color, label)
 end
+
+# And then plot the evolution of the observed fields,
 
 fig, axs = make_figure_axes()
 
-for i = 1:length(observations.times)
-    b = observations.field_time_serieses.b[i]
-    e = observations.field_time_serieses.e[i]
-    u = observations.field_time_serieses.u[i]
-    v = observations.field_time_serieses.v[i]
-    t = observations.times[i]
-
-    label = "t = " * prettytime(t)
-    u_label = i == 1 ? "u, " * label : label
-    v_label = i == 1 ? "v, " * label : label
-    plot_fields!(axs, b, u, v, e, label, u_label, v_label, colorcycle[i])
+for (i, t) in enumerate(times)
+    fields = map(name -> observations.field_time_serieses[name][i], field_names)
+    plot_fields!(axs, fields..., "t = " * prettytime(t), colorcycle[i])
 end
 
-axislegend(axs[1], position=:rb)
-axislegend(axs[2], position=:lb, merge=true)
-axislegend(axs[3], position=:rb)
+[axislegend(ax, position=:rb, merge=true, fontsize=12) for ax in axs]
 
+display(fig)
 save("lesbrary_synthetic_observations.svg", fig); nothing # hide
 
 # ![](lesbrary_synthetic_observations.svg)
 
-# Well, that looks like a boundary layer.
+# Behold, boundary layer turbulence!
 # 
 # # Calibration
 #
 # Next, we build a simulation of an ensemble of column models to calibrate
-# CATKE using Ensemble Kalman Inversion.
+# CATKE using Ensemble Kalman Inversion. We configure CATKE without convective
+# adjustment and with constant (rather than Richardson-number-dependent)
+# diffusivity parameters.
 
 catke_mixing_length = MixingLength(Cᴷcʳ=0.0, Cᴷuʳ=0.0, Cᴷeʳ=0.0)
 catke = CATKEVerticalDiffusivity(mixing_length=catke_mixing_length)
@@ -108,15 +100,18 @@ N² .= observations.metadata.parameters.N²_deep
 # We identify a subset of the CATKE parameters to calibrate by specifying
 # parameter names and prior distributions:
 
-priors = (Cᴰ   = lognormal_with_mean_std(2.5,  0.1),
-          Cᵂu★ = lognormal_with_mean_std(1.0,  0.1),
-          CᵂwΔ = lognormal_with_mean_std(0.05, 0.1),
-          Cᴸᵇ  = lognormal_with_mean_std(0.05, 0.1),
-          Cᴷu⁻ = ConstrainedNormal(0.1,  0.05, 0.0, 2.0),
-          Cᴷc⁻ = ConstrainedNormal(0.1,  0.05, 0.0, 2.0),
-          Cᴷe⁻ = ConstrainedNormal(1.0,  0.1,  0.0, 2.0))
+priors = (Cᴰ   = lognormal_with_mean_std(2.0, 0.5),
+          Cᵂu★ = lognormal_with_mean_std(2.0, 0.5),
+          Cᴸᵇ  = lognormal_with_mean_std(0.2, 0.1),
+          Cᴷu⁻ = ConstrainedNormal(0.2, 0.1, 0.0, 2.0),
+          Cᴷc⁻ = ConstrainedNormal(1.0, 0.1, 0.0, 2.0),
+          Cᴷe⁻ = ConstrainedNormal(1.5, 0.3, 0.0, 2.0))
 
 free_parameters = FreeParameters(priors)
+
+# TODO: explain the meaning of each parameter
+# The prior information comes from experience, prior calibration runs,
+# and educated guesses.
 
 calibration = InverseProblem(observations, simulation, free_parameters)
 
@@ -125,80 +120,61 @@ calibration = InverseProblem(observations, simulation, free_parameters)
 
 eki = EnsembleKalmanInversion(calibration;
                               noise_covariance = 1e-1,
-                              resampler = NaNResampler(abort_fraction=0.1))
+                              resampler = NaNResampler(abort_fraction=0.5))
 
-iterate!(eki; iterations = 1)
-
-# One iteration won't do much. But let's look at the results anyways
-# by executing another run with the ensemble mean parameters:
-
-best_parameters = eki.iteration_summaries[end].ensemble_mean
-forward_run!(calibration, best_parameters)
+# We also build some utilities for comparing observed and model output later on:
 
 Nt = length(observations.times)
-tN = observations.times[Nt]
-
-b_obs = observations.field_time_serieses.b[Nt]
-e_obs = observations.field_time_serieses.e[Nt]
-u_obs = observations.field_time_serieses.u[Nt]
-v_obs = observations.field_time_serieses.v[Nt]
-
-model_time_serieses = calibration.time_series_collector.field_time_serieses 
-b_model = model_time_serieses.b[Nt]
-e_model = model_time_serieses.e[Nt]
-u_model = model_time_serieses.u[Nt]
-v_model = model_time_serieses.v[Nt]
+modeled_time_serieses = calibration.time_series_collector.field_time_serieses 
+observed = map(name -> observations.field_time_serieses[name][Nt], field_names)
+modeled = map(name -> modeled_time_serieses[name][Nt], field_names)
 
 function compare_model_observations()
     fig, axs = make_figure_axes()
-       
-    color = :black
-    label = "observed at t = " * prettytime(tN)
-    plot_fields!(axs, b_obs, u_obs, v_obs, e_obs, label, "u " * label, "v " * label, color)
-    
-    color = :blue
-    label = "modeled"
-    plot_fields!(axs, b_model, u_model, v_model, e_model, label, "u " * label, "v " * label, color)
-        
-    axislegend(axs[1], position=:rb)
-    axislegend(axs[2], position=:lb, merge=true)
-    axislegend(axs[3], position=:rb)
-
+    plot_fields!(axs, observed..., "observed at t = " * prettytime(times[end]), :black)
+    plot_fields!(axs, modeled..., "modeled", :blue)
+    [axislegend(ax, position=:rb, merge=true, fontsize=12) for ax in axs]
     return fig
 end
 
-fig = compare_model_observations()
+# Now let's iterate to see if we can find better parameters than our initial guess...
 
-save("model_observation_comparison_iteration_1.svg", fig); nothing # hide
+iterate!(eki; iterations = 5)
 
-# ![](model_observation_comparison_iteration_1.svg)
+# To evaluate how 
 
-# Now let's see if further iterations improve that result...
-
-iterate!(eki; iterations = 10)
+initial_parameters = eki.iteration_summaries[0].ensemble_mean
 best_parameters = eki.iteration_summaries[end].ensemble_mean
+
+forward_run!(calibration, initial_parameters)
+fig = compare_model_observations()
+display(fig)
+save("model_observation_comparison_iteration_0.svg", fig); nothing # hide
+
 forward_run!(calibration, best_parameters)
 fig = compare_model_observations()
+display(fig)
+save("model_observation_comparison_iteration_10.svg", fig); nothing # hide
 
 # Let's see how the parameters evolved:
 
-summaries = eki.iteration_summaries
-parameter_names = keys(first(summaries).ensemble_mean)
+ensemble_means = NamedTuple(n => map(summary -> summary.ensemble_mean[n], eki.iteration_summaries)
+                            for n in calibration.free_parameters.names)
 
-ensemble_means = NamedTuple(name => map(summary -> summary.ensemble_mean[name], summaries)
-                            for name in parameter_names)
 fig = Figure()
 ax = Axis(fig[1, 1], xlabel = "Iteration", ylabel = "Parameter value")
+iterations = 0:length(eki.iteration_summaries)-1
 
-for (i, name) in enumerate(parameter_names)
+for (i, name) in enumerate(calibration.free_parameters.names)
     label = string(name)
     marker = markercycle[i]
     color = colorcycle[i]
-    scatterlines!(ax, 0:length(summaries)-1, parent(ensemble_means[name]); marker, color, label)
+    scatterlines!(ax, iterations, parent(ensemble_means[name]); marker, color, label)
 end
 
 axislegend(ax, position=:rb)
+display(fig)
+save("lesbrary_catke_parameter_evolution.svg", fig); nothing # hide
 
-save("model_observation_comparison_iteration_12.svg", fig); nothing # hide
+# ![](lesbrary_catke_parameter_evolution.svg)
 
-# ![](model_observation_comparison_iteration_12.svg)
