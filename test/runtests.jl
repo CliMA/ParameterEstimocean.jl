cc24b8519727ec8210c20654edbbff215f6fb96f
Navid C. Constantinou <navidcy@users.noreply.github.com>
2022-02-11 06:16:41 +1100

    bring back all tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,16 @@
-using Documenter, Test, OceanTurbulenceParameterEstimation
+using Documenter, Test
 
-# include("test_lesbrary_datadeps.jl")
-# include("test_synthetic_observations.jl")
-# include("test_transformations.jl")
-# include("test_parameters.jl")
-# include("test_forward_map.jl")
-# include("test_lesbrary_forward_map.jl")
-# include("test_ensemble_column_models.jl")
-# include("test_ensemble_kalman_inversion.jl")
+include("test_lesbrary_datadeps.jl")
+include("test_synthetic_observations.jl")
+include("test_transformations.jl")
+include("test_parameters.jl")
+include("test_forward_map.jl")
+include("test_lesbrary_forward_map.jl")
+include("test_ensemble_column_models.jl")
+include("test_ensemble_kalman_inversion.jl")
 
 @testset "Doctests" begin
-    using Distributions
+    using OceanTurbulenceParameterEstimation, Distributions
+    
     doctest(OceanTurbulenceParameterEstimation)
 end
