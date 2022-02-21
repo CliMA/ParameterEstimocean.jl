5201d3676b2c12083a6978df9b3f25d60e65eb34
Navid C. Constantinou <navidcy@users.noreply.github.com>
2022-02-21 21:42:55 +1100

    adjust pdf plots lims

--- examples/perfect_baroclinic_adjustment_calibration.jl
+++ examples/perfect_baroclinic_adjustment_calibration.jl
@@ -326,8 +326,8 @@ xlims!(axmain, 350, 1350)
 xlims!(axtop, 350, 1350)
 ylims!(axmain, 650, 1750)
 ylims!(axright, 650, 1750)
-xlims!(axright, 0, 0.025)
-ylims!(axtop, 0, 0.025)
+xlims!(axright, 0, 0.02s)
+ylims!(axtop, 0, 0.02)
 
 save("distributions_baroclinic_adjustment.svg", f); nothing #hide 
 
