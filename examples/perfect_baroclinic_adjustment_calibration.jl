ef5931ff4c73b39f06bd1d729f3add172393c008
Navid C. Constantinou <navidcy@users.noreply.github.com>
2022-02-21 19:15:49 +1100

    increase convergence rate

--- examples/perfect_baroclinic_adjustment_calibration.jl
+++ examples/perfect_baroclinic_adjustment_calibration.jl
@@ -233,7 +233,7 @@ mean(G, dims=2) ≈ y
 
 # Next, we construct an `EnsembleKalmanInversion` (EKI) object,
 
-eki = EnsembleKalmanInversion(calibration; convergence_rate = 0.6)
+eki = EnsembleKalmanInversion(calibration; convergence_rate = 0.7)
 
 # and perform few iterations to see if we can converge to the true parameter values.
 
