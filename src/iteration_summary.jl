a8eb6d71a5db591bffcd47a2623003b0d6c2bd69
Adeline Hillier <51460867+adelinehillier@users.noreply.github.com>
2022-03-24 13:48:57 -0400

    Update src/iteration_summary.jl
    Co-authored-by: Navid C. Constantinou <navidcy@users.noreply.github.com>

--- src/iteration_summary.jl
+++ src/iteration_summary.jl
@@ -133,4 +133,3 @@ particle_str(particle, error, parameters) =
     @sprintf("% 11s particle: ", particle) *
     string(param_str.(values(parameters))...) *
     @sprintf("error = %.6e", error)
-    
