f3c7c07df3461134e447fe5c68e849d9df12272e
Gregory Wagner <wagner.greg@gmail.com>
2022-04-05 20:42:03 -0600

    Replace CATKE example with Ri-based example in docs

--- docs/make.jl
+++ docs/make.jl
@@ -26,7 +26,8 @@ to_be_literated = [
   "exploring_priors.jl",
   "perfect_convective_adjustment_calibration.jl",
   # "perfect_catke_calibration.jl",
-  "lesbrary_catke_calibration.jl",
+  #"lesbrary_catke_calibration.jl",
+  "lesbrary_ri_based_calibration.jl",
   "perfect_baroclinic_adjustment_calibration.jl"
 ]
 
@@ -60,7 +61,8 @@ pages = [
     "Examples" => [ 
         "literated/perfect_convective_adjustment_calibration.md",
         # "literated/perfect_catke_calibration.md",
-        "literated/lesbrary_catke_calibration.md",
+        #"literated/lesbrary_catke_calibration.md",
+        "literated/lesbrary_ri_based_calibration.md",
         "literated/perfect_baroclinic_adjustment_calibration.md"
         ],
     
