fcc1eafd8d4a3af9ad9c1f08b7e9f68ce14ca006
Navid C. Constantinou <navidcy@users.noreply.github.com>
2022-10-30 08:02:57 +1100

    ommit Ri-based diffusivity examples

--- docs/make.jl
+++ docs/make.jl
@@ -25,8 +25,8 @@ to_be_literated = [
   "intro_to_inverse_problems.jl",
   "exploring_priors.jl",
   "perfect_convective_adjustment_calibration.jl",
-  "single_case_lesbrary_ri_based_calibration.jl",
-  "multi_case_lesbrary_ri_based_calibration.jl",
+  # "single_case_lesbrary_ri_based_calibration.jl",
+  # "multi_case_lesbrary_ri_based_calibration.jl",
   "perfect_baroclinic_adjustment_calibration.jl"
 ]
 
