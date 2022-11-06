2f0036b42407724aaa9757eb6381b03bc665ec4e
Navid C. Constantinou <navidcy@users.noreply.github.com>
2022-11-06 16:49:47 +1100

    bring back all tests for ensemble models

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,7 +6,7 @@ include("test_transformations.jl")
 include("test_parameters.jl")
 include("test_forward_map.jl")
 include("test_lesbrary_forward_map.jl")
-include("test_ensemble_column_models.jl")
+include("test_ensemble_models.jl")
 include("test_ensemble_kalman_inversion.jl")
 include("test_utils.jl")
 
